d471d0dabd0dd95300beca5209652bf4c2acf086
Sergusha-97 <sergeybirich@gmail.com>
2016-11-28 20:19:33 +0300

    Overrided methods Equals and GetHashCode for using in Dictionary

--- Mapper.UnitTests/Mapper/MappingElement.cs
+++ Mapper.UnitTests/Mapper/MappingElement.cs
@@ -10,5 +10,30 @@ namespace Mapper
     {
         public Type Source { get; set; }
         public Type Destination { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this == obj) return true;
+            var comparrigMappingElement = obj as MappingElement;
+            if (comparrigMappingElement == null) return false;
+            return IsElementsEqual(comparrigMappingElement);
+        }
+
+        private bool IsElementsEqual(MappingElement obj)
+        {
+            return this.Source == obj.Source &&
+                   this.Destination == obj.Destination;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked 
+            {
+                int hash = 17;
+                hash = hash * 23 + (Source == null ? 0 : Source.GetHashCode());
+                hash = hash * 23 + (Destination == null ? 0 : Destination.GetHashCode());
+                return hash;
+            }
+        }
     }
 }
